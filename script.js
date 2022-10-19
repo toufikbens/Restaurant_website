a215b6d3d3e9f1144200d46492453a0af2b5bf89
Toufik Bensalem <toufik24051994@gmail.com>
2022-10-19 15:11:12 +0100

    Restaurant website page i did as a learning project

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById("eat").addEventListener(
+  "mouseover",
+  function () {
+    document.getElementById("background").style.backgroundImage =
+      "url(https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)";
+  },
+  false
+);
+document.getElementById("drink").addEventListener(
+  "mouseover",
+  function () {
+    document.getElementById("background").style.backgroundImage =
+      "url(https://images.pexels.com/photos/605408/pexels-photo-605408.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)";
+  },
+  false
+);
+document.getElementById("visit").addEventListener(
+  "mouseover",
+  function () {
+    document.getElementById("background").style.backgroundImage =
+      "url(https://images.pexels.com/photos/6341164/pexels-photo-6341164.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)";
+  },
+  false
+);
+document.getElementById("background").addEventListener(
+  "mouseover",
+  function () {
+    document.getElementById("background").style.backgroundImage =
+      "url(https://images.pexels.com/photos/2983101/pexels-photo-2983101.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)";
+  },
+  false
+);
